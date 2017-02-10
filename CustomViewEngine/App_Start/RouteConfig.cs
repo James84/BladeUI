db8d4925dc102887e3e0e38d3b6207e9f5326c98
James Runham <james.runham@reedonline.co.uk>
2017-02-10 14:05:02 +0000

    first commit

---
+++ CustomViewEngine/App_Start/RouteConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace CustomViewEngine.UI.App_Start
+{
+    public static class RouteConfig
+    {
+        public static void Register(RouteCollection routes)
+        {
+            routes.MapRoute("Home", "{controller}/{action}/{id}", new { controller = "home", action = "index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ CustomViewEngine/ViewEngine/CustomViewEngine.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace CustomViewEngine.UI.ViewEngine
+{
+    public class CustomViewEngine : VirtualPathProviderViewEngine
+    {
+        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
