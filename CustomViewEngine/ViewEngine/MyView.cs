785eb79b9faae813eac5a73d57cb68ed113b871e
James Runham <james.runham@reedonline.co.uk>
2017-02-10 14:47:40 +0000

    first commit

---
+++ CustomViewEngine/ViewEngine/MyView.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace CustomViewEngine.UI.ViewEngine
+{
+    public class MyView : IView
+    {
+        private string _viewPhysicalPath;
+
+        public MyView(string viewPhysicalPath)
+        {
+            _viewPhysicalPath = viewPhysicalPath;
+        }
+
+        public void Render(ViewContext viewContext, TextWriter writer)
+        {
+            var rawContents = File.ReadAllText(_viewPhysicalPath);
+
+            var parsedContents = Parse(rawContents, viewContext.ViewData);
+
+            writer.Write(parsedContents);
+        }
+
+        public string Parse(string contents, ViewDataDictionary viewData)
+        {
+            return Regex.Replace(contents, "\\{(.+)\\}", m => GetMatch(m, viewData));
+        }
+
+        public virtual string GetMatch(Match match, ViewDataDictionary viewData)
+        {
+            if (match.Success)
+            {
+                var key = match.Result("$1");
+
+                if (viewData.ContainsKey(key))
+                {
+                    return viewData[key].ToString();
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ CustomViewEngine/ViewEngine/MyViewEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace CustomViewEngine.UI.ViewEngine
+{
+    public class MyViewEngine : VirtualPathProviderViewEngine
+    {
+        public MyViewEngine()
+        {
+            ViewLocationFormats = new[] {"~/Views/{1}/{0}.myview", "~/Views/Shared/{0}.myview"};
+            PartialViewLocationFormats = new[] {"~/Views/{1}/{0}", "~/Views/Shared/{0}.myview"};
+        }
+
+        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+        {
+            var physicalPath = controllerContext.HttpContext.Server.MapPath(partialPath);
+            return new MyView(physicalPath);
+        }
+
+        protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+        {
+            var physicalPath = controllerContext.HttpContext.Server.MapPath(viewPath);
+            return new MyView(physicalPath);
+        }
+    }
+}
